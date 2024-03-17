832012fb0cf26c189f75bf059ab566363c706960
niraj <nky.jh14@gmail.com>
2024-03-17 09:47:34 +0530

    docker file

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,13 @@
 import path from "path"
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
+import reactRefresh from '@vitejs/plugin-react-refresh'
 
 export default defineConfig({
+  server: {
+    host: '0.0.0.0',
+    port: 3000,
+  },
   plugins: [react()],
   resolve: {
     alias: {
